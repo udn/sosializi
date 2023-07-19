8578e0a86cd745ad7c151ba410e70ef1e8b5cc26
burhanudin hakim <udnpico@gmail.com>
2023-07-19 15:50:10 +0700

    first commit

---
+++ urlshortener/urlshortener_generator.go
@@ -0,0 +1,31 @@
+package urlshortener
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+	"os"
+)
+
+func GenerateShortLink(initialLink string, userId string) string {
+	urlHashBytes := sha256Of(initialLink + userId)
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+	return finalString[:8]
+}
+
+func sha256Of(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
